407e7d81e9fb2e020d3cd22a9eb67cb2fc403469
coDribble <sksrlatkddn@naver.com>
2019-07-22 14:42:40 +0900

    190722 include test

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+	<meta name="title" content="coDribble">
+	<meta name="keywords" content="coDribble">
+	<meta name="description" content="coDribble">
+
+	<title>coDribble</title>
+
+	<link rel="stylesheet" type="text/css" href="./assets/css/layout.css">
+</head>
+<body>
+
+<div class="container">
